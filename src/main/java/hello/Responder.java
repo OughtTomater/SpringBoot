8c09685bc9b7eb51000c7bf5df52ab0e2e2de23c
OughtTomater <30742080+OughtTomater@users.noreply.github.com>
2017-08-05 00:40:57 -0400

    First
    first commit

---
+++ src/main/java/hello/Responder.java
@@ -0,0 +1,21 @@
+package hello;
+
+public class Responder {
+
+    private final long id;
+    private final String content;
+
+    public Responder(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
