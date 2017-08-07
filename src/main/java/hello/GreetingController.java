4d2ba89bf1cf0db70bc4930407e91d37374a12da
OughtTomater <30742080+OughtTomater@users.noreply.github.com>
2017-08-06 22:51:47 -0400

    page and form service
    added page and form service plus free template CSS

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -2,8 +2,7 @@ package hello;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -13,10 +12,22 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    // for Spring MVC Example
+ /*   @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
+    }*/
+
+    @GetMapping("/greeting")
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting";
+    }
+
+    @PostMapping("/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting) {
+        return "result";
     }
 
 }
